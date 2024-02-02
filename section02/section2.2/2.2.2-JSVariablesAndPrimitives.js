fd23262b4c9ce70431cf9d78520a1ac1a044088f
delorean williams <deloreanwilliams25@gmail.com>
2024-02-01 19:17:09 -0600

    classActivities

---
+++ section02/section2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,37 @@
+// create a variable called "name" that references a string
+let name = "Delorean";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "middle child";
+// create a variable called "wage" that references a number
+let wage = 10;
+// create a variable called "age" that references a number
+let age = 30;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = "basketball";
+console.log(typeof favoriteThing);
+
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = goodmorning;
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = "warriors";
+// Happy Birthday!
+// Reassign "age" to a new value
+age = 31;
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 11;
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = false;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("My name is" + name + "and my favoriteSong is " + favoriteSong);
+
+let str = "";
+let num = 4;
