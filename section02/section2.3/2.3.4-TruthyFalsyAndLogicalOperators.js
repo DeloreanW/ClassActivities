b3764cd6ce448f907b35eea47fa9a9cadfcf7ade
delorean williams <deloreanwilliams25@gmail.com>
2024-02-01 20:02:07 -0600

    lesson on for & wwhile loops

---
+++ section02/section2.3/2.3.4-TruthyFalsyAndLogicalOperators.js
@@ -0,0 +1,24 @@
+console.log(true && false);
+console.log(false && false);
+console.log(!true && true);
+
+console.log(false || !false);
+
+console.log(!(true && false));
+console.log(!(falsse));
+console.log(!false)
+
+if (!isAuthenticated(user))
+
+console.log{!(1 && "false")}
+console.log{"false" && true};
+
+let age = 12;
+let menu = age > 10 ? "full menu" ; "kids menu";
+
+let num= 0;
+if (false || num) {
+  console.log("the number is zero")
+}
+console.log("the number is zero")
+
